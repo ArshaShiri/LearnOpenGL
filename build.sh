94ad6d3e8e3ebddf3f4e8dc37e49065f3824a67f
Arsha <arsha.shiri@gmail.com>
2024-01-23 20:49:26 +0100

    build: fix the build script

--- build.sh
+++ build.sh
@@ -10,34 +10,47 @@
 # sudo apt install libxcb-cursor-dev
 # sudo apt install libxcb-util-dev
 
+progname="$(basename "$0")"
+
 # TODO Add more options to the build: Debug etc.
 printManual() {
+    cat <<EOF
+usage: $progname
+
     echo -c "\t clean build"
-    exit 1
+    echo -d "\t clean build"
+EOF
+    exit "${1-0}"
 }
 
-while getopts ":c" opt; do
+while getopts ci opt; do
     case $opt in
-    c) clean_build=true ;;
-    i) install_conan=true ;;
-    \?)
+    c)
+        clean_build=true
+        ;;
+    i)
+        install_conan=true
+        ;;
+    *)
         printManual
-        exit
+        exit 64 # EX_USAGE
         ;;
     esac
 done
 
 build_folder_name="build"
 
-if [ $install_conan ]; then
+if [ $clean_build ]; then
+    rm -rf $build_folder_name
+fi
+
+if [ "$install_conan" ] || [ "$clean_build" ]; then
     conan create lib/glad/.
     conan install . --build=missing
-    # conan install . --output-folder=$build_folder_name --build=missing
 fi
 
 cd build/Release/generators
 if [ $clean_build ]; then
-    rm -rf $build_folder_name
     cmake ../../.. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 fi
 
