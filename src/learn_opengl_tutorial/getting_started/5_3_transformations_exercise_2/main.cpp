72460817ad114e3890c0d3a94ca19f39887b9c50
Arsha <arsha.shiri@gmail.com>
2024-02-08 21:56:04 +0100

    feat: add 5_3_transformations_exercise_2

--- src/learn_opengl_tutorial/getting_started/5_3_transformations_exercise_2/main.cpp
+++ src/learn_opengl_tutorial/getting_started/5_3_transformations_exercise_2/main.cpp
@@ -1,5 +1,7 @@
-// Using the last transformation on the container, try switching the order around by first rotating and then
-// translating. See what happens and try to reason why this happens
+// Try drawing a second container with another call to glDrawElements but place it at a different position using
+// transformations only. Make sure this second container is placed at the top-left of the window and instead of
+// rotating, scale it over time (using the sin function is useful here; note that using sin will cause the object to
+// invert as soon as a negative scale is applied)
 
 #include <filesystem>
 #include <iostream>
@@ -36,11 +38,11 @@ int main()
     // TODO Having the path like this is quite fragile since it depends on the path from which the binary is run.
     // TODO It is much better if the shader paths are passed as command line arguments.
     const auto vertexShaderFilePath = std::filesystem::current_path().append(
-      "src/learn_opengl_tutorial/getting_started/5_2_transformations_exercise_1/vertex_shader.vs");
+      "src/learn_opengl_tutorial/getting_started/5_1_transformations/vertex_shader.vs");
     const auto vertexShader = Shader(ShaderType::Vertex, "", vertexShaderFilePath);
 
     const auto fragmentShaderFilePath = std::filesystem::current_path().append(
-      "src/learn_opengl_tutorial/getting_started/5_2_transformations_exercise_1/fragment_shader.fs");
+      "src/learn_opengl_tutorial/getting_started/5_1_transformations/fragment_shader.fs");
     const auto fragmentShader = Shader(ShaderType::Fragment, "", fragmentShaderFilePath);
 
     auto shaderProgram = Program();
@@ -170,35 +172,22 @@ int main()
         GLCall(glActiveTexture(GL_TEXTURE1));
         GLCall(glBindTexture(GL_TEXTURE_2D, texture2));
 
+        // Spinning logic
+        trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, static_cast<float>(glfwGetTime()), glm::vec3(0.0f, 0.0f, 1.0f));
+        shaderProgram.setMatrix4fv("transform", trans);
+
         renderer.draw(vertexArrayObject, indexBuffer, shaderProgram);
 
-        // Spinning logic
-
-        /* Why does our container now spin around our screen?:
-        == ===================================================
-        Remember that matrix multiplication is applied in reverse. This time a translation is thus
-        applied first to the container positioning it in the bottom-right corner of the screen.
-        After the translation the rotation is applied to the translated container.
-
-        A rotation transformation is also known as a change-of-basis transformation
-        for when we dig a bit deeper into linear algebra. Since we're changing the
-        basis of the container, the next resulting translations will translate the container
-        based on the new basis vectors. Once the vector is slightly rotated, the vertical
-        translations would also be slightly translated for example.
-
-        If we would first apply rotations then they'd resolve around the rotation origin (0,0,0), but
-        since the container is first translated, its rotation origin is no longer (0,0,0) making it
-        looks as if its circling around the origin of the scene.
-
-        If you had trouble visualizing this or figuring it out, don't worry. If you
-        experiment with transformations you'll soon get the grasp of it; all it takes
-        is practice and experience.
-        */
-
+        // second transformation
         trans = glm::mat4(1.0f);
-        trans = glm::rotate(trans, static_cast<float>(glfwGetTime()), glm::vec3(0.0f, 0.0f, 1.0f));
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+        float scaleAmount = static_cast<float>(sin(glfwGetTime()));
+        trans = glm::scale(trans, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
         shaderProgram.setMatrix4fv("transform", trans);
+
+        renderer.draw(vertexArrayObject, indexBuffer, shaderProgram);
 
         window.swapBuffers();
         glfwPollEvents();
